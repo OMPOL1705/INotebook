5d1d62bc292f21c394d31d7ffbabc7a208417905
OM1705 <ompol1705@gmail.com>
2023-07-02 19:00:20 +0530

    context api

---
+++ src/context/notes/NoteState.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NoteContext from "./NotesContext";
+
+const NoteState = (props) => {
+    return (
+        <NoteContext.Provider value={[]}>
+            {props.children}
+        </NoteContext.Provider>
+    )
+
+}
+
+export default NoteState;
