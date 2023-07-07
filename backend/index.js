ed55b5bd43f6c251302dc4958955040fae5e8d5a
OM1705 <ompol1705@gmail.com>
2023-07-07 15:06:05 +0530

    oijij

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 const connectToMongo = require('./db');
-const path = require('path');
 const express = require('express');
 var cors = require('cors');
 
@@ -13,11 +12,6 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
