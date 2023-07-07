43ad89412475dea08b767ce2a2a0fb0d1a8354e3
OM1705 <ompol1705@gmail.com>
2023-07-07 14:46:22 +0530

    iuihuh

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const connectToMongo = require('./db');
+const path = require('path');
 const express = require('express');
 var cors = require('cors');
 
@@ -6,17 +7,17 @@ connectToMongo();
 const app = express();
 const port = 5000;
 
-app.use(cors(
-  {
-    origin: ["https://i-notebook-api-one.vercel.app"],
-    methods: ["POST", "GET", "DELETE", "PUT"],
-    credentials: true
-  }
-))
+app.use(cors())
 app.use(express.json())
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
